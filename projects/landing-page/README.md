5f6277c68e82f79c17031f97e3f3a0e74a3ed8f7
Nihith <sainihith9618@gmail.com>
2022-06-12 00:40:25 +0530

    Landing Page Project files

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -1,13 +1,35 @@
 # Landing Page Project
 
+  
+
 ## Table of Contents
 
-* [Instructions](#instructions)
+*  [Description](#description)
+*  [Instructions](#instructions)
+*  [Dependencies](#dependencies)
+
+
+
+## Description
+This Project tests your knowledge on the basic building blocks of Frontend Web Development such as HTML, CSS and JavaScript.
+
+Make sure you revise ES6 features, DOM Traversal, DOM Manipulation and 
+Event Handling before attempting the project.
 
 ## Instructions
 
 The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
+  
+
 To get started, open `js/app.js` and start building out the app's functionality
 
+  
+
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Dependencies
+To run and test the project you just need to have the following:
+
+	- A Modern Web Browser such as Chrome, Firefox or Edge.
+	- An IDE such as Visual Studio Code, Atom or Sublime to make development faster and easier.
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -22,7 +22,8 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section')
+const navBarList = document.querySelector('#navbar__list')
 
 /**
  * End Global Variables
@@ -39,10 +40,60 @@
 */
 
 // build the nav
+function buildNavigationBar(){
+    /**
+     * @description A function that dynamically creates the Navigation Bar based on the number of sections present in the page
+    */
+
+    //Iterate through all the sections
+    for(let section of sections){
+        const sectionID = section.id
+        const sectionName = section.getAttribute('data-nav')
+        const newListItem = document.createElement('li')
+        const newMenuElement = document.createElement('a')
+        newMenuElement.innerText = sectionName
+        newMenuElement.setAttribute('data-section-id',sectionID)
+        newMenuElement.classList.add('menu__link')
+        //Defines an event listener for click event 
+        newMenuElement.addEventListener('click', (event) => {
+            event.preventDefault()
+            section.scrollIntoView({behavior:'smooth'})
+        })
+        //Appends the new elements to the DOM
+        newListItem.appendChild(newMenuElement)
+        navBarList.appendChild(newListItem)
+    }
+
+    addScrollToSections() 
+}
 
 
 // Add class 'active' to section when near top of viewport
+function addScrollToSections(){
+    /**
+     * @description A function which highlights the section and the navigation menu when user scrolls through the page
+    */
 
+    window.addEventListener('scroll', () => {
+        //Iterate through all the sections 
+        for(let section of sections){
+            const sectionBound = section.getBoundingClientRect()
+            const sectionID = section.id
+            const navMenuElement = document.querySelector(`a[data-section-id="${sectionID}"]`)
+            //Checks whether the section rectangle bounds are in the user's visible view port or not
+            if(sectionBound.top >= 0 && sectionBound.top < window.innerHeight && sectionBound.bottom <= window.innerHeight){
+                section.classList.add('active')
+                navMenuElement.classList.add('menu__link_active')
+            } else {
+                section.classList.remove('active')
+                navMenuElement.classList.remove('menu__link_active')
+            }
+        }
+    })
+}
+
+
+buildNavigationBar();
 
 // Scroll to anchor ID using scrollTO event
 
